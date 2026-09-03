7effc1a266e9147b32f516b126489b1b222918e6
daavinskiy <1avdeev.daniil@gmail.com>
2026-09-03 14:45:07 +0300

    tehtava 1.14 on valmis

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -2,47 +2,54 @@ import { useState } from 'react'
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
+    'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
 
   const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const totalVotes = votes.reduce((sum, vote) => sum + vote, 0)
+
+  const maxVotes = Math.max(...votes)
+  const maxIndex = votes.indexOf(maxVotes)
 
   const vote = () => {
-    const newVotes = [...votes]
-    newVotes[selected] += 1
-    setVotes(newVotes)
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
   }
 
   const nextAnecdote = () => {
-    const random = Math.floor(Math.random() * anecdotes.length)
-    setSelected(random)
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
   }
 
   return (
     <div>
-      <p>
-        {anecdotes[selected]}
-      </p>
+      <h1>Anecdote of the day</h1>
 
-      <p>
-        has {votes[selected]} votes
-      </p>
+      <p>{anecdotes[selected]}</p>
 
-      <button onClick={vote}>
-        vote
-      </button>
+      <p>has {votes[selected]} votes</p>
 
-      <button onClick={nextAnecdote}>
-        next anecdote
-      </button>
+      <button onClick={vote}>vote</button>
+
+      <button onClick={nextAnecdote}>next anecdote</button>
+
+      {totalVotes > 0 && (
+        <div>
+          <h2>Anecdote with most votes</h2>
+
+          <p>{anecdotes[maxIndex]}</p>
+
+          <p>has {maxVotes} votes</p>
+        </div>
+      )}
     </div>
   )
 }
