887e49adbd23d6499f3a81a50cc5a4ef12be253e
daavinskiy <1avdeev.daniil@gmail.com>
2026-09-14 10:21:56 +0300

    tehtava 2.13 on valmis

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const Filter = ({ search, handleSearchChange }) => {
   return (
@@ -64,10 +64,10 @@ const App = () => {
   const [search, setSearch] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(data => {
+        setPersons(data)
       })
   }, [])
 
@@ -98,10 +98,10 @@ const App = () => {
       number: newNumber
     }
 
-    axios
-      .post('http://localhost:3001/persons', personObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    personService
+      .create(personObject)
+      .then(data => {
+        setPersons(persons.concat(data))
         setNewName('')
         setNewNumber('')
       })
