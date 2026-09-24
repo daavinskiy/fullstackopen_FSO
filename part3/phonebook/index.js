9ffd4a4c63a6ba3c89379ac45a776a8fff72571b
daavinskiy <1avdeev.daniil@gmail.com>
2026-09-24 08:40:55 +0300

    tehtava 3.4 on valmis

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -47,6 +47,14 @@ app.get('/api/info', (request, response) => {
   `
 
   response.send(info)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
